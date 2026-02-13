91c54f361b8870bec9b797865d428cefcde15fbd
Null Pointer <nullpointer1101@outlook.com>
2026-02-13 12:59:07 +0530

    pipa: Inherit keys only if they are not included before.

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -11,11 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-# Inherit from pipa device
+# Inherit from pipa device.
 $(call inherit-product, device/xiaomi/pipa/device.mk)
 
-# Inherit keys
+# Inherit keys only if they are not included before.
+ifndef ANDROID_PRIV_KEYS_MK_INCLUDED
 $(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+endif
 
 # Derpfest Flags
 DERPFEST_BUILD_TYPE := Official
